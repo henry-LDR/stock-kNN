f96f454b2462ca87d5d78e536a3bc6836e7c029f
Henry <hji@opticaplp.com>
2025-07-22 23:17:12 -0400

    github

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-#Helper functions
+#Data
+winners=['MGNI','CYTK','CI', 'GOGO','FIVN','ESTA']
+candidates=['XYZ','PI','MS','MTCH','TMDX','TXRH']
+backtest=['MSFT']
+#Parameters
 metrics=['beta','forwardPE','marketCap','volume','targetMeanPrice','revenueGrowth']
+
+#Helper functions
 def data(stocks,metrics):
     stockmetrics=[]
     for i in range(len(stocks)):
@@ -25,8 +31,6 @@ def data(stocks,metrics):
 #TODO: Create a screener that generates a list of preliminary candidates-these will get k-NN
 
 #Set up the PCA:
-#Array with stocks that made good pitches
-winners=['MGNI','CYTK','CI', 'GOGO','FIVN','ESTA']
 #Then generate a numpy matrix with row[i] containing all of the metrics for stock[i]
 #For instance, P/E, EV/EBITDA, etc.
 #Important: How to deal with negative or nonexistent multiples? Cannot just have them in.
@@ -48,26 +52,32 @@ pcasm=pca.transform(scaledsm)
 print(scaledsm)
 print('pca')
 print(pcasm)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter3D(pcasm[:,0], pcasm[:,1], pcasm[:,2])
-
 
 #TODO: Use k-Nearest Neighbors to compare the candidates against winners
 #TODO: What are some good metrics to compare? We need them to do the PCA.
 ##TODO: We need to see which metrics are clustered for winning stock pitches? Use PCA to do so and weigh them.
 #TODO: After we get the PCA matrix and loading, we can apply it to new stocks and get k-NN scores
 
-#Initialize an array of candidates
-candidates=['XYZ','PI','MS','MTCH','TMDX','TXRH']
-#Collect the same data
+#Collect the same data for candidates
 candidatematrix=data(candidates,metrics)
 #Now standardize:
 scalar.fit(candidatematrix)
 scalar.transform(candidatematrix)
 proj=pca.transform(candidatematrix)
 
+
+#Backtests
+backtestmatrix=data(backtest,metrics)
+scalar.fit(backtestmatrix)
+scalar.transform(backtestmatrix)
+back=pca.transform(backtestmatrix)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter3D(pcasm[:,0], pcasm[:,1], pcasm[:,2])
 ax.scatter3D(proj[:,0], proj[:,1], proj[:,2],'r')
+ax.scatter3D(back[:,0], back[:,1], back[:,2],'y')
 plt.show()
+
 
 print('works')
